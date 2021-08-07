25cc708ee8fa5177bdbd8c5cfbcc9661c5c3d004
DOMO <Domaslu98@gmail.com>
2021-08-07 14:31:42 +0300

    Score tracking and calculation

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,20 +1,33 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Clock))]
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
+    int score = 0;
 
+    Clock clock;
     void Awake()
     {
         if (Instance != null)
             Destroy(gameObject);
         Instance = this;
+        MapGrid.OnFoodTaken += UpdateScore;
+        clock = GetComponent<Clock>();
     }
 
     public void TriggerGameOver()
     {
         Clock.PauseClock();
+        UI.Instance.SetScore(score);
         UI.Instance.GameOverObject.SetActive(true);
         // TODO: add score to local leaderboard
+    }
+
+    void UpdateScore() => score += Mathf.FloorToInt(5 / clock.ClockSpeed);
+
+    private void OnDestroy()
+    {
+        MapGrid.OnFoodTaken -= UpdateScore;
     }
 }
